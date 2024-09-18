f94bff1dd809b1941d119a7ab08400516a474270
Apiwat Lee <apiwatz.lee@gmail.com>
2024-09-18 17:49:51 +0700

    vuejs fundamental

---
+++ src/App2.vue
@@ -0,0 +1,65 @@
+<script setup>
+import { onMounted, ref } from "vue";
+
+const name = ref("John Doe");
+const status = ref("active");
+const tasks = ref([]);
+const newTask = ref("");
+
+const toggleStatus = () => {
+  if (status.value === "active") {
+    status.value = "pending";
+  } else if (status.value === "pending") {
+    status.value = "inactive";
+  } else {
+    status.value = "active";
+  }
+};
+
+const addTask = () => {
+  if (newTask.value.trim() !== "") {
+    tasks.value.push(newTask.value);
+    newTask.value = "";
+  }
+};
+
+const deleteTask = (index) => {
+  tasks.value.splice(index, 1);
+};
+
+onMounted(async () => {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+    const data = await response.json();
+    tasks.value = data;
+  } catch (error) {
+    console.log(error);
+  }
+});
+</script>
+
+<template>
+  <div>{{ name }}</div>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else>User is inactive</p>
+
+  <form @submit.prevent="addTask">
+    <label for="newTask">Add task</label>
+    <input type="text" id="newTask" name="newTask" v-model="newTask" />
+    <button type="submit">Submit</button>
+  </form>
+
+  <h3>Tasks:</h3>
+  <ul>
+    <li v-for="(task, index) in tasks" :key="task">
+      <span>{{ task?.title }}</span>
+      <button @click="deleteTask(index)">x</button>
+    </li>
+  </ul>
+
+  <button @click="toggleStatus">change status</button>
+</template>
+
+<!-- scope meaning scope css for the component -->
+<style scoped></style>
